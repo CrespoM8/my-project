f355ff6b09a99e8190591febe9515bd28b3c4900
CrespoM8 <alumno.708781@ies-azarquiel.es>
2025-11-24 18:15:16 +0100

    último commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,22 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
+const isBuild = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
-  root: "./src", // no olvidemos cambiar la raíz del proyecto a "src"
+  // Solo usa root en desarrollo, no en producción
+  root: isBuild ? undefined : "./src",
+
   build: {
+    outDir: "dist", // Esto genera la carpeta en la raíz del proyecto
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "./src/index.html"), // Página principal
-        about: resolve(__dirname, "./src/formulario.html"), // Otra página
+        main: resolve(__dirname, "src/index.html"), // Ruta absoluta desde la raíz
+        about: resolve(__dirname, "src/formulario.html"),
       },
     },
   },
-  base: "./", // Opcional: rutas relativas para mayor compatibilidad
+
+  base: "/my-project/",
 });
